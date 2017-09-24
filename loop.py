eb8bc98584e962ed234238e6cdec772a6b38521c
Abhinand N <01abhinandn@gmail.com>
2017-09-24 11:35:05 +0530

    Adding loop

---
+++ loop.py
@@ -0,0 +1,22 @@
+
+#while loop
+
+count = 0
+while (count < 9):
+    print 'The count is:', count
+    count = count + 1
+
+print "Good bye!"
+
+#    for loop
+
+for i in range(0,10):     # First Example
+    print i
+
+fruits = ['banana', 'apple',  'mango']
+
+#  for loop
+for fruit in fruits:        # Second Example
+    print 'Current fruit :', fruit
+
+print "Good bye!"
